72ee7af0dd4661879fc0186a9517578a21ab70bb
yaojiach <jiachen.yao@outlook.com>
2018-09-23 13:03:04 -0400

    Fix structure of run_job_flow template

--- red_panda/emr_admin_templates.py
+++ red_panda/emr_admin_templates.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
+STR = 'string'
+NUM = 'number'
 EMR_RUN_JOB_FLOW_TEMPLATE = {
     # required
-    'Name': 'string',
+    'Name': STR,
     'Instances': {
-        'MasterInstanceType': 'string',
-        'SlaveInstanceType': 'string',
-        'InstanceCount': 123,
+        'MasterInstanceType': STR,
+        'SlaveInstanceType': STR,
+        'InstanceCount': NUM,
         'InstanceGroups': [
             {
-                'Name': 'string',
-                'Market': 'ON_DEMAND'|'SPOT',
-                'InstanceRole': 'MASTER'|'CORE'|'TASK',
-                'BidPrice': 'string',
-                'InstanceType': 'string',
-                'InstanceCount': 123,
+                'Name': STR,
+                'Market': {'ON_DEMAND', 'SPOT'},
+                'InstanceRole': {'MASTER', 'CORE', 'TASK'},
+                'BidPrice': STR,
+                'InstanceType': STR,
+                'InstanceCount': NUM,
                 'Configurations': [
                     {
-                        'Classification': 'string',
-                        'Configurations': {'... recursive ...'},
+                        'Classification': STR,
+                        'Configurations': {STR},
                         'Properties': {
-                            'string': 'string'
+                            STR: STR
                         }
                     },
                 ],
@@ -27,46 +29,89 @@ EMR_RUN_JOB_FLOW_TEMPLATE = {
                     'EbsBlockDeviceConfigs': [
                         {
                             'VolumeSpecification': {
-                                'VolumeType': 'string',
-                                'Iops': 123,
-                                'SizeInGB': 123
+                                'VolumeType': STR,
+                                'Iops': NUM,
+                                'SizeInGB': NUM
                             },
-                            'VolumesPerInstance': 123
+                            'VolumesPerInstance': NUM
                         },
                     ],
-                    'EbsOptimized': True|False
+                    'EbsOptimized': {True, False}
                 },
                 'AutoScalingPolicy': {
                     'Constraints': {
-                        'MinCapacity': 123,
-                        'MaxCapacity': 123
+                        'MinCapacity': NUM,
+                        'MaxCapacity': NUM
                     },
                     'Rules': [
                         {
-                            'Name': 'string',
-                            'Description': 'string',
+                            'Name': STR,
+                            'Description': STR,
                             'Action': {
-                                'Market': 'ON_DEMAND'|'SPOT',
+                                'Market': {'ON_DEMAND', 'SPOT'},
                                 'SimpleScalingPolicyConfiguration': {
-                                    'AdjustmentType': 'CHANGE_IN_CAPACITY'|'PERCENT_CHANGE_IN_CAPACITY'|'EXACT_CAPACITY',
-                                    'ScalingAdjustment': 123,
-                                    'CoolDown': 123
+                                    'AdjustmentType': {
+                                        'CHANGE_IN_CAPACITY',
+                                        'PERCENT_CHANGE_IN_CAPACITY',
+                                        'EXACT_CAPACITY'
+                                    },
+                                    'ScalingAdjustment': NUM,
+                                    'CoolDown': NUM
                                 }
                             },
                             'Trigger': {
                                 'CloudWatchAlarmDefinition': {
-                                    'ComparisonOperator': 'GREATER_THAN_OR_EQUAL'|'GREATER_THAN'|'LESS_THAN'|'LESS_THAN_OR_EQUAL',
-                                    'EvaluationPeriods': 123,
-                                    'MetricName': 'string',
-                                    'Namespace': 'string',
-                                    'Period': 123,
-                                    'Statistic': 'SAMPLE_COUNT'|'AVERAGE'|'SUM'|'MINIMUM'|'MAXIMUM',
-                                    'Threshold': 123.0,
-                                    'Unit': 'NONE'|'SECONDS'|'MICRO_SECONDS'|'MILLI_SECONDS'|'BYTES'|'KILO_BYTES'|'MEGA_BYTES'|'GIGA_BYTES'|'TERA_BYTES'|'BITS'|'KILO_BITS'|'MEGA_BITS'|'GIGA_BITS'|'TERA_BITS'|'PERCENT'|'COUNT'|'BYTES_PER_SECOND'|'KILO_BYTES_PER_SECOND'|'MEGA_BYTES_PER_SECOND'|'GIGA_BYTES_PER_SECOND'|'TERA_BYTES_PER_SECOND'|'BITS_PER_SECOND'|'KILO_BITS_PER_SECOND'|'MEGA_BITS_PER_SECOND'|'GIGA_BITS_PER_SECOND'|'TERA_BITS_PER_SECOND'|'COUNT_PER_SECOND',
+                                    'ComparisonOperator': {
+                                        'GREATER_THAN_OR_EQUAL',
+                                        'GREATER_THAN',
+                                        'LESS_THAN',
+                                        'LESS_THAN_OR_EQUAL'
+                                    },
+                                    'EvaluationPeriods': NUM,
+                                    'MetricName': STR,
+                                    'Namespace': STR,
+                                    'Period': NUM,
+                                    'Statistic': {
+                                        'SAMPLE_COUNT',
+                                        'AVERAGE',
+                                        'SUM',
+                                        'MINIMUM',
+                                        'MAXIMUM'
+                                    },
+                                    'Threshold': NUM,
+                                    'Unit': {
+                                        'NONE',
+                                        'SECONDS',
+                                        'MICRO_SECONDS',
+                                        'MILLI_SECONDS',
+                                        'BYTES',
+                                        'KILO_BYTES',
+                                        'MEGA_BYTES',
+                                        'GIGA_BYTES',
+                                        'TERA_BYTES',
+                                        'BITS',
+                                        'KILO_BITS',
+                                        'MEGA_BITS',
+                                        'GIGA_BITS',
+                                        'TERA_BITS',
+                                        'PERCENT',
+                                        'COUNT',
+                                        'BYTES_PER_SECOND',
+                                        'KILO_BYTES_PER_SECOND',
+                                        'MEGA_BYTES_PER_SECOND',
+                                        'GIGA_BYTES_PER_SECOND',
+                                        'TERA_BYTES_PER_SECOND',
+                                        'BITS_PER_SECOND',
+                                        'KILO_BITS_PER_SECOND',
+                                        'MEGA_BITS_PER_SECOND',
+                                        'GIGA_BITS_PER_SECOND',
+                                        'TERA_BITS_PER_SECOND',
+                                        'COUNT_PER_SECOND'
+                                    },
                                     'Dimensions': [
                                         {
-                                            'Key': 'string',
-                                            'Value': 'string'
+                                            'Key': STR,
+                                            'Value': STR
                                         },
                                     ]
                                 }
@@ -77,41 +122,41 @@ EMR_RUN_JOB_FLOW_TEMPLATE = {
             },
         ],
         # optional
-        'LogUri': 'string',
-        'AdditionalInfo': 'string',
-        'AmiVersion': 'string',
-        'ReleaseLabel': 'string',
+        'LogUri': STR,
+        'AdditionalInfo': STR,
+        'AmiVersion': STR,
+        'ReleaseLabel': STR,
         'InstanceFleets': [
             {
-                'Name': 'string',
-                'InstanceFleetType': 'MASTER'|'CORE'|'TASK',
-                'TargetOnDemandCapacity': 123,
-                'TargetSpotCapacity': 123,
+                'Name': STR,
+                'InstanceFleetType': {'MASTER', 'CORE', 'TASK'},
+                'TargetOnDemandCapacity': NUM,
+                'TargetSpotCapacity': NUM,
                 'InstanceTypeConfigs': [
                     {
-                        'InstanceType': 'string',
-                        'WeightedCapacity': 123,
-                        'BidPrice': 'string',
-                        'BidPriceAsPercentageOfOnDemandPrice': 123.0,
+                        'InstanceType': STR,
+                        'WeightedCapacity': NUM,
+                        'BidPrice': STR,
+                        'BidPriceAsPercentageOfOnDemandPrice': NUM,
                         'EbsConfiguration': {
                             'EbsBlockDeviceConfigs': [
                                 {
                                     'VolumeSpecification': {
-                                        'VolumeType': 'string',
-                                        'Iops': 123,
-                                        'SizeInGB': 123
+                                        'VolumeType': STR,
+                                        'Iops': NUM,
+                                        'SizeInGB': NUM
                                     },
-                                    'VolumesPerInstance': 123
+                                    'VolumesPerInstance': NUM
                                 },
                             ],
-                            'EbsOptimized': True|False
+                            'EbsOptimized': {True, False}
                         },
                         'Configurations': [
                             {
-                                'Classification': 'string',
-                                'Configurations': {'... recursive ...'},
+                                'Classification': STR,
+                                'Configurations': {STR},
                                 'Properties': {
-                                    'string': 'string'
+                                    STR: STR
                                 }
                             },
                         ]
@@ -119,119 +164,124 @@ EMR_RUN_JOB_FLOW_TEMPLATE = {
                 ],
                 'LaunchSpecifications': {
                     'SpotSpecification': {
-                        'TimeoutDurationMinutes': 123,
-                        'TimeoutAction': 'SWITCH_TO_ON_DEMAND'|'TERMINATE_CLUSTER',
-                        'BlockDurationMinutes': 123
+                        'TimeoutDurationMinutes': NUM,
+                        'TimeoutAction': {'SWITCH_TO_ON_DEMAND', 'TERMINATE_CLUSTER'},
+                        'BlockDurationMinutes': NUM
                     }
                 }
             },
         ],
-        'Ec2KeyName': 'string',
+        'Ec2KeyName': STR,
         'Placement': {
-            'AvailabilityZone': 'string',
+            'AvailabilityZone': STR,
             'AvailabilityZones': [
-                'string',
+                STR,
             ]
         },
-        'KeepJobFlowAliveWhenNoSteps': True|False,
-        'TerminationProtected': True|False,
-        'HadoopVersion': 'string',
-        'Ec2SubnetId': 'string',
+        'KeepJobFlowAliveWhenNoSteps': {True, False},
+        'TerminationProtected': {True, False},
+        'HadoopVersion': STR,
+        'Ec2SubnetId': STR,
         'Ec2SubnetIds': [
-            'string',
+            STR,
         ],
-        'EmrManagedMasterSecurityGroup': 'string',
-        'EmrManagedSlaveSecurityGroup': 'string',
-        'ServiceAccessSecurityGroup': 'string',
+        'EmrManagedMasterSecurityGroup': STR,
+        'EmrManagedSlaveSecurityGroup': STR,
+        'ServiceAccessSecurityGroup': STR,
         'AdditionalMasterSecurityGroups': [
-            'string',
+            STR,
         ],
         'AdditionalSlaveSecurityGroups': [
-            'string',
+            STR,
         ]
     },
     'Steps': [
         {
-            'Name': 'string',
-            'ActionOnFailure': 'TERMINATE_JOB_FLOW'|'TERMINATE_CLUSTER'|'CANCEL_AND_WAIT'|'CONTINUE',
+            'Name': STR,
+            'ActionOnFailure': {
+                'TERMINATE_JOB_FLOW',
+                'TERMINATE_CLUSTER',
+                'CANCEL_AND_WAIT',
+                'CONTINUE'
+            },
             'HadoopJarStep': {
                 'Properties': [
                     {
-                        'Key': 'string',
-                        'Value': 'string'
+                        'Key': STR,
+                        'Value': STR
                     },
                 ],
-                'Jar': 'string',
-                'MainClass': 'string',
+                'Jar': STR,
+                'MainClass': STR,
                 'Args': [
-                    'string',
+                    STR,
                 ]
             }
         },
     ],
     'BootstrapActions': [
         {
-            'Name': 'string',
+            'Name': STR,
             'ScriptBootstrapAction': {
-                'Path': 'string',
+                'Path': STR,
                 'Args': [
-                    'string',
+                    STR,
                 ]
             }
         },
     ],
     'SupportedProducts': [
-        'string',
+        STR,
     ],
     'NewSupportedProducts': [
         {
-            'Name': 'string',
+            'Name': STR,
             'Args': [
-                'string',
+                STR,
             ]
         },
     ],
     'Applications': [
         {
-            'Name': 'string',
-            'Version': 'string',
+            'Name': STR,
+            'Version': STR,
             'Args': [
-                'string',
+                STR,
             ],
             'AdditionalInfo': {
-                'string': 'string'
+                STR: STR
             }
         },
     ],
     'Configurations': [
         {
-            'Classification': 'string',
-            'Configurations': {'... recursive ...'},
+            'Classification': STR,
+            'Configurations': {STR},
             'Properties': {
-                'string': 'string'
+                STR: STR
             }
         },
     ],
-    'VisibleToAllUsers': 'True|False',
-    'JobFlowRole': 'string',
-    'ServiceRole': 'string',
+    'VisibleToAllUsers': {True, False},
+    'JobFlowRole': STR,
+    'ServiceRole': STR,
     'Tags': [
         {
-            'Key': 'string',
-            'Value': 'string'
+            'Key': STR,
+            'Value': STR
         },
     ],
-    'SecurityConfiguration': 'string',
-    'AutoScalingRole': 'string',
-    'ScaleDownBehavior': "'TERMINATE_AT_INSTANCE_HOUR'|'TERMINATE_AT_TASK_COMPLETION'",
-    'CustomAmiId': 'string',
-    'EbsRootVolumeSize': 123,
-    'RepoUpgradeOnBoot': "'SECURITY'|'NONE'",
+    'SecurityConfiguration': STR,
+    'AutoScalingRole': STR,
+    'ScaleDownBehavior': {'TERMINATE_AT_INSTANCE_HOUR', 'TERMINATE_AT_TASK_COMPLETION'},
+    'CustomAmiId': STR,
+    'EbsRootVolumeSize': NUM,
+    'RepoUpgradeOnBoot': {'SECURITY', 'NONE'},
     'KerberosAttributes': {
-        'Realm': 'string',
-        'KdcAdminPassword': 'string',
-        'CrossRealmTrustPrincipalPassword': 'string',
-        'ADDomainJoinUser': 'string',
-        'ADDomainJoinPassword': 'string'
+        'Realm': STR,
+        'KdcAdminPassword': STR,
+        'CrossRealmTrustPrincipalPassword': STR,
+        'ADDomainJoinUser': STR,
+        'ADDomainJoinPassword': STR
     }
 }
