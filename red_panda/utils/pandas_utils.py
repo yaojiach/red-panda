37e559d6f9a603884247d5e44218b7b9b217b231
yaojiach <jiachen.yao@outlook.com>
2019-03-14 11:12:44 -0400

    Add row_number in pandas_utils

--- red_panda/utils/pandas_utils.py
+++ red_panda/utils/pandas_utils.py
@@ -7,3 +7,8 @@ def merge_dfs(dfs, **kwargs):
     """Merge a list of DataFrames on common columns
     """
     return reduce(lambda df1, df2: pd.merge(df1, df2, **kwargs), dfs)
+
+def row_number(df, group_by, sort_by, ascending=True):
+    """Create a row number series given a DataFrame lists of columns for group by and sort by
+    """
+    return df.sort_values(sort_by, ascending=ascending).groupby(group_by).cumcount()
