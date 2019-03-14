e149f838332912d088d709df6b9f6fb90063090a
yaojiach <jiachen.yao@outlook.com>
2019-03-14 14:00:26 -0400

    Add groupby_mutate in pandas_utils

--- red_panda/utils/pandas_utils.py
+++ red_panda/utils/pandas_utils.py
@@ -12,3 +12,17 @@ def row_number(df, group_by, sort_by, ascending=True):
     """Create a row number series given a DataFrame lists of columns for group by and sort by
     """
     return df.sort_values(sort_by, ascending=ascending).groupby(group_by).cumcount()
+
+def groupby_mutate(df, group_by, mutate):
+    """Similar to R's dplyr::mutate
+
+    # Example
+        ```python
+        def def func(g):
+            return g['b'] / g['b'].sum()
+        
+        df['pct'] = groupby_mutate(df, ['a'], func)
+        ```
+    """
+    # return df.groupby(group_by, group_keys=False).apply(lambda g: eval(mutate))
+    return df.groupby(group_by, group_keys=False).apply(mutate)
