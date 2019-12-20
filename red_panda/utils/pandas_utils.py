a18760876e8b5070f1db9378d2ec1ad10b942cc3
yaojiach <jiachen.yao@outlook.com>
2019-12-20 15:46:29 -0500

    Add groupby utils for pandas

--- red_panda/utils/pandas_utils.py
+++ red_panda/utils/pandas_utils.py
@@ -8,13 +8,15 @@ def merge_dfs(dfs, **kwargs):
     """
     return reduce(lambda df1, df2: pd.merge(df1, df2, **kwargs), dfs)
 
+
 def row_number(df, group_by, sort_by, ascending=True):
     """Create a row number series given a DataFrame lists of columns for group by and sort by
     """
     return df.sort_values(sort_by, ascending=ascending).groupby(group_by).cumcount()
 
-def groupby_mutate(df, group_by, mutate):
-    """Similar to R's dplyr::mutate
+
+def _groupby_mutate(df, group_by, mutate):
+    """Internal base function similar to R's dplyr::mutate
 
     # Example
         ```python
@@ -26,3 +28,43 @@ def groupby_mutate(df, group_by, mutate):
     """
     # return df.groupby(group_by, group_keys=False).apply(lambda g: eval(mutate))
     return df.groupby(group_by, group_keys=False).apply(mutate)
+
+
+def groupby_mutate(df, group_by, func_dict):
+    """Similar to R's dplyr::mutate
+
+    # Example
+        ```python
+        def func(x):
+            return x['a'].nunique() / x['b'].nunique()
+        func_dict = {
+            'a_u': lambda x: x['a'].nunique(),
+            'c': func
+        }
+        # achieve the same:
+        def f(x):
+            d = {}
+            d['a_u'] = x['a'].nunique()
+            d['c'] = x['a'].nunique() / x['b'].nunique()
+            return pd.Series(d, index=['a_u', 'c'])
+        _groupby_mutate(df, ['group'], f).reset_index()
+        ```
+    """
+    return _groupby_mutate(
+        df,
+        group_by,
+        lambda x: pd.Series(
+            { col: func(x) for col, func in func_dict.items() },
+            index=list(func_dict.keys())
+        )
+    ).reset_index()
+
+
+def groupby_distinct(df, group_by, distinct):
+    func_dict = {}
+    if isinstance(distinct, list):
+        for d in distinct:
+            func_dict[d] = lambda x: x[d].nunique()
+    else:
+        func_dict[distinct] = lambda x: x[distinct].nunique()
+    return groupby_mutate(df, group_by, func_dict)
