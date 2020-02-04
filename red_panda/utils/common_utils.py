02a4a7c734d6c13326cb8187741e2cd8e68f42d5
yaojiach <jiachen.yao@outlook.com>
2020-02-04 15:41:20 -0500

    Add flatten

--- red_panda/utils/common_utils.py
+++ red_panda/utils/common_utils.py
@@ -2,6 +2,7 @@
 import os
 import re
 from copy import deepcopy
+from typing import Iterable
 
 
 def filter_kwargs(full, ref):
@@ -11,6 +12,7 @@ def filter_kwargs(full, ref):
 def prettify_sql(sql):
     return re.sub(r'\n\s*\n*', '\n', sql.lstrip())
 
+
 def make_valid_uri(*args):
     if len(args) >= 2:
         l = deepcopy(list(args))
@@ -18,3 +20,14 @@ def make_valid_uri(*args):
         if i[0] == '/':
             l[1] = i[1:]
     return os.path.join(*l)
+
+
+def flatten(items):
+    """Yield items from any nested iterable; see Reference.
+    """
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            for sub_x in flatten(x):
+                yield sub_x
+        else:
+            yield x
