041b34b89c588b31b2c70eca3cda915f089ee6b1
yaojiach <jiachen.yao@outlook.com>
2020-08-18 23:59:40 -0400

    Add some s3 integ tests

--- tests/integ/aws/test_s3.py
+++ tests/integ/aws/test_s3.py
@@ -10,5 +10,18 @@ LOGGER = logging.getLogger(__name__)
 def s3_utils(aws_config):
     return S3Utils(aws_config)
 
+
+def test_check_s3_bucket_existence_on_existing_bucket(s3_utils, s3_bucket):
+    assert s3_utils._check_s3_bucket_existence(s3_bucket)
+
+
 def test_check_s3_bucket_existence_on_non_existent_bucket(s3_utils):
-    assert not s3_utils._check_s3_bucket_existence("SHOULDNOTEXIST")
+    assert not s3_utils._check_s3_bucket_existence("BUCKET_SHOULD_NOT_EXIST")
+
+
+def test_check_s3_key_existence_on_non_existent_key(s3_utils, s3_bucket):
+    assert not s3_utils._check_s3_key_existence(s3_bucket, "KEY_SHOULD_NOT_EXIST")
+
+
+def test_list_buckets(s3_utils, s3_bucket):
+    assert s3_bucket in s3_utils.list_buckets()
