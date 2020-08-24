29b579f317a21a65807e8c24c1fb05f4ed74ddb1
yaojiach <jiachen.yao@outlook.com>
2020-08-24 00:21:46 -0400

    Add tests

--- tests/unit/aws/test_s3.py
+++ tests/unit/aws/test_s3.py
@@ -1,9 +1,11 @@
 import pytest
-from moto import mock_s3
+from moto.s3 import mock_s3
+import boto3
 
 from red_panda.aws.s3 import S3Utils
 
 import logging
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,10 +16,22 @@ MOCK_AWS_CONFIG = {
 
 
 @pytest.fixture
-@mock_s3
 def s3_utils():
-    return S3Utils(MOCK_AWS_CONFIG)
+    LOGGER.info(f">>>>> Setup mock S3")
+    with mock_s3():
+        s3 = boto3.resource("s3")
+        s3.create_bucket(Bucket="EXISTINGBUCKET")
+        yield S3Utils(MOCK_AWS_CONFIG)
+    LOGGER.info(f">>>>> Teardown mock S3")
 
 
 def test_check_s3_bucket_existence_on_non_existent_bucket(s3_utils):
     assert not s3_utils._check_s3_bucket_existence("DOESNOTEXIST")
+
+
+def test_check_s3_bucket_existence_on_existing_bucket(s3_utils):
+    assert s3_utils._check_s3_bucket_existence("EXISTINGBUCKET")
+
+
+def test_list_buckets(s3_utils):
+    assert "EXISTINGBUCKET" in s3_utils.list_buckets()
